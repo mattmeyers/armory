0927079e5c80e3b2151b3a7e8abaf4c293383edf
Chris Meyers <cm.02.93@gmail.com>
2020-04-21 22:02:20 -0600

    added queue documentation.

--- queue/queue.go
+++ queue/queue.go
@@ -5,14 +5,18 @@ import (
 	"strings"
 )
 
+// Generic aliases the empty interface.
 type Generic interface{}
 
+// Zero is the default zero value for a queue.
 var Zero Generic = nil
 
+// GenericQueue implements a queue data structure with a slice.
 type GenericQueue struct {
 	vals []Generic
 }
 
+// NewGenericQueue creates a new queue of generic values.
 func NewGenericQueue(vals ...Generic) *GenericQueue {
 	q := &GenericQueue{vals: []Generic{}}
 
@@ -37,14 +41,17 @@ func (q *GenericQueue) String() string {
 	return sb.String()
 }
 
+// IsEmpty checks if the queue does not contain any elements.
 func (q *GenericQueue) IsEmpty() bool {
 	return len(q.vals) == 0
 }
 
+// Push appends an element to the queue.
 func (q *GenericQueue) Push(val Generic) {
 	q.vals = append(q.vals, val)
 }
 
+// Pop removes the element in the front of the queue and returns the value.
 func (q *GenericQueue) Pop() Generic {
 	if q.IsEmpty() {
 		return Zero
